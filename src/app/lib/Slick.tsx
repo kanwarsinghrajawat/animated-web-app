a0947ef84abcec3aadf81cde2b315059fb15b10f
Kanwar Singh <kanwarsinghrajawat3@gmail.com>
2024-04-27 23:26:29 +0530

    initial-commit

---
+++ src/app/lib/Slick.tsx
@@ -0,0 +1,42 @@
+"use client";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function Carousel() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    centerMode: true,
+    centerPadding: "100",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+        },
+      },
+    ],
+  };
+  return (
+    <Slider {...settings} className="carousel-container">
+      <div>
+        <img src="/9.png" alt="" />
+      </div>
+      <div>
+        <img src="/10.png" alt="" />
+      </div>
+      <div>
+        <img src="/11.png" alt="" />
+      </div>
+      <div>
+        <img src="/12.png" alt="" />
+      </div>
+    </Slider>
+  );
+}
